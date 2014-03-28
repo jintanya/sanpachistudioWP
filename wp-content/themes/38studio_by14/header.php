d9d3e15f6982ede2602e7b632458d49c66a2bbed
AkikoYoshioka <jintan@jintanya.net>
2014-03-29 08:07:47 +0900

    38スタジオ用のテンプレ作成

---
+++ wp-content/themes/38studio_by14/header.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * The Header for our theme
+ *
+ * Displays all of the <head> section and everything up till <div id="main">
+ *
+ * @package WordPress
+ * @subpackage Twenty_Fourteen
+ * @since Twenty Fourteen 1.0
+ */
+?><!DOCTYPE html>
+<!--[if IE 7]>
+<html class="ie ie7" <?php language_attributes(); ?>>
+<![endif]-->
+<!--[if IE 8]>
+<html class="ie ie8" <?php language_attributes(); ?>>
+<![endif]-->
+<!--[if !(IE 7) | !(IE 8) ]><!-->
+<html <?php language_attributes(); ?>>
+<!--<![endif]-->
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width">
+<link rel="stylesheet" href="css/style-base.css" />
+	<title><?php the_title(); ?>｜<?php wp_title(); ?></title>
+	<!--[if lt IE 9]>
+	<script src="<?php echo get_template_directory_uri(); ?>/js/html5.js"></script>
+	<![endif]-->
+<script type="text/javascript" src="js/rollover.js"></script>
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-41379702-1', '38studio.com');
+  ga('send', 'pageview');
+
+</script>
+</head>
+
+<body <?php body_class(); ?>>
+<div class="wrapper">
---
+++ wp-content/themes/38studio_by14/index.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * The main template file
+ *
+ * This is the most generic template file in a WordPress theme and one
+ * of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query,
+ * e.g., it puts together the home page when no home.php file exists.
+ *
+ * @link http://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package WordPress
+ * @subpackage Twenty_Fourteen
+ * @since Twenty Fourteen 1.0
+ */
+
+get_header(); ?>
+
+<table class="index_menu" border="0" cellpadding="0" cellspacing="0">
+	<tr>
+		<td>
+			<img src="<?php bloginfo('template_directory'); ?>/images/1_on.jpg" />
+		</td>
+	</tr>
+	<tr>
+		<td><a href="schedule/index.html"><img src="<?php bloginfo('template_directory'); ?>/images/2_off.jpg" /></a></td>
+	</tr>
+	<tr>
+		<td>
+		<a href="map.html">
+			<img src="<?php bloginfo('template_directory'); ?>/images/3_off.jpg" /></a>
+		</td>
+	</tr>
+		<tr>
+			<td>
+				<a href="gohan/recipe/index.html">
+				<img src="<?php bloginfo('template_directory'); ?>/images/8_off.jpg" /></a>
+			</td>
+		</tr>
+	<!--tr>
+		<td><a href="webshop/index.html"><img src="images/6_off.jpg"/></a></td>
+	</tr-->
+</table>
+
+		<?php query_posts('cat=2&post_type=post&posts_per_page=1&paged='.$paged); ?>
+
+					<?php if (have_posts()) : ?>
+						<?php while (have_posts()) : the_post(); ?>
+								<div class="index_news">
+									<p class="index_date"><?php the_title(); ?></p>
+									<br />
+									<?php the_content(); ?>
+								</div>
+						<?php endwhile; ?>
+					<?php else : ?>
+							 
+					<?php endif; ?>	
+
+<?php
+get_sidebar();
+get_footer();
